43dcb7a00c323942e15ff1f3d378ab24f50a997f
rparkins999 <aleph0hpela-bughit@yahoo.co.uk>
2024-09-12 14:36:33 +0100

    Script to make a new app icon with build time and date

---
+++ SecondsClock/gimpscript.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+gimp -i -b - <<end
+; Create object 1 as a new image
+(gimp-image-new 144 144 0)
+; White on black, not black on white
+(gimp-context-swap-colors)
+; Create object 2 as a text layer in that image
+(gimp-text-layer-new 1 "20:04:56\n$1\n12\nSeptember\n2024" "arial" 25 0)
+; Add the layer to the image
+(gimp-image-insert-layer 1 2 0 0)
+; Centre the text
+(gimp-text-layer-set-justification 2 2)
+(define x (quotient (- (car (gimp-image-width 1)) (car (gimp-drawable-width 2))) 2))
+(gimp-layer-set-offsets 2 x 0)
+; Force black backgground
+(gimp-layer-flatten 2)
+; Froce width to fit image width
+(gimp-layer-resize-to-image-size 2)
+; Export the icon
+(file-png-save 1 1 2 "ic_launcher.png" "ic_launcher.png" 0 0 0 0 0 0 0)
+; Exit
+(gimp-quit TRUE)
+end
